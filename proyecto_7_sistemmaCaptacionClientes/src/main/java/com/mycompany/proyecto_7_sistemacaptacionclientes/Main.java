3e97005bfa579ef0c583fa21e19593204125c324
angegon <angegon@gmail.com>
2023-01-30 23:46:33 +0100

    formularios

---
+++ proyecto_7_sistemmaCaptacionClientes/src/main/java/com/mycompany/proyecto_7_sistemacaptacionclientes/Main.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.proyecto_7_sistemacaptacionclientes;
+
+/**
+ *
+ * @author angegon
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        
+        ventana.show();
+    }
+}
